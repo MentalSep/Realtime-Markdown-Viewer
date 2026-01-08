052bafecc012f474c8da23a7b6b9d3e347715bdc
MentalSeppuku <chahidjava@hotmail.com>
2026-01-08 09:23:47 +0100

    add link to parser

--- converter/markdown.js
+++ converter/markdown.js
@@ -1,28 +1,46 @@
- var parseHeadline = function(str) {
-   var headlineRegExp =  /^(\#{1,6})([^\#\n]+)$/m;
-   var stra = [];
-   while ((stra = headlineRegExp.exec(str)) !== null) {
-     count = stra[1].length;
-     str = str.replace(stra[0], '<h' + count + '>' + stra[2] + '</h' + count + '>' + '\n');
-   }
-   return str;
- }
+var parseHeadline = function (str) {
+  var headlineRegExp = /^(\#{1,6})([^\#\n]+)$/m;
+  var stra = [];
+  while ((stra = headlineRegExp.exec(str)) !== null) {
+    count = stra[1].length;
+    str = str.replace(
+      stra[0],
+      "<h" + count + ">" + stra[2] + "</h" + count + ">" + "\n"
+    );
+  }
+  return str;
+};
+
+
+parseHorizontaleLine = function (str) {
+  var horizontalRegExp = /^(?:([\*\-_] ?)+)\1\1$/gm;
+  var stra = [];
+  while ((stra = horizontalRegExp.exec(str)) !== null) {
+    str = str.replace(stra[0], "\n<hr/>\n");
+  }
+  return str;
+};
+
+var parseLink = function (str) {
+  var linkRegExp = /\[([^\[]+)\]\(([^\)]+)\)/;
+  var stra = [];
+  while ((stra = linkRegExp.exec(str)) !== null) {
+    str = str.replace(
+      stra[0],
+      "<a " + 'href="' + stra[2] + '">' + stra[1] + "</a>"
+    );
+  }
+  return str;
+};
 
 var markdown = {
   parse: function (str, strict) {
-    'use strict';
+    "use strict";
     str = parseHeadline(str);
     str = parseHorizontaleLine(str);
+    str = parseLink(str);
     return str;
-  }
+  },
 };
- parseHorizontaleLine = function(str) {
- var horizontalRegExp = /^(?:([\*\-_] ?)+)\1\1$/gm;
- var stra = [];
- while ((stra = horizontalRegExp.exec(str)) !== null) {
- str = str.replace(stra[0], '\n<hr/>\n');
- }
- return str;
-}
 
 module.exports = markdown;
