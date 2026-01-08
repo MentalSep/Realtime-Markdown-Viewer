9ea6d32d5725957bc720bd81649a59bba6614846
MentalSeppuku <chahidjava@hotmail.com>
2026-01-08 09:27:18 +0100

    add bold to parser

--- converter/markdown.js
+++ converter/markdown.js
@@ -1,28 +1,41 @@
- var parseHeadline = function(str) {
-   var headlineRegExp =  /^(\#{1,6})([^\#\n]+)$/m;
-   var stra = [];
-   while ((stra = headlineRegExp.exec(str)) !== null) {
-     count = stra[1].length;
-     str = str.replace(stra[0], '<h' + count + '>' + stra[2] + '</h' + count + '>' + '\n');
-   }
-   return str;
- }
+var parseHeadline = function (str) {
+  var headlineRegExp = /^(\#{1,6})([^\#\n]+)$/m;
+  var stra = [];
+  while ((stra = headlineRegExp.exec(str)) !== null) {
+    count = stra[1].length;
+    str = str.replace(
+      stra[0],
+      "<h" + count + ">" + stra[2] + "</h" + count + ">" + "\n"
+    );
+  }
+  return str;
+};
 
 var markdown = {
   parse: function (str, strict) {
-    'use strict';
+    "use strict";
     str = parseHeadline(str);
+    str = parseBold(str);
     str = parseHorizontaleLine(str);
     return str;
-  }
+  },
 };
- parseHorizontaleLine = function(str) {
- var horizontalRegExp = /^(?:([\*\-_] ?)+)\1\1$/gm;
- var stra = [];
- while ((stra = horizontalRegExp.exec(str)) !== null) {
- str = str.replace(stra[0], '\n<hr/>\n');
- }
- return str;
-}
+parseHorizontaleLine = function (str) {
+  var horizontalRegExp = /^(?:([\*\-_] ?)+)\1\1$/gm;
+  var stra = [];
+  while ((stra = horizontalRegExp.exec(str)) !== null) {
+    str = str.replace(stra[0], "\n<hr/>\n");
+  }
+  return str;
+};
+
+var parseBold = function (str) {
+  var boldRegExp = /(\*\*)(.*?)\1/;
+  var stra = [];
+  while ((stra = boldRegExp.exec(str)) !== null) {
+    str = str.replace(stra[0], "<strong>" + stra[2] + "</strong>");
+  }
+  return str;
+};
 
 module.exports = markdown;
